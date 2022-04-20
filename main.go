53e5591f8aa03250b3b187348380bc167ccab3b7
lprmrl <lcthulhu666l@gmail.com>
2022-04-20 14:47:03 +0300

    test echoString

--- main.go
+++ main.go
@@ -5,14 +5,13 @@ import (
 	"log"
 	"net/http"
 )
-func echoString(w http.ResponseWriter, r *http.Request,) {
+
+func echoString(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello my friend")
 }
 
-
 func main() {
 	http.HandleFunc("/", echoString)
-	})
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Server running")
